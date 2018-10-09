1d5e64bd721bf9b2bbd91a0ccfd15c7c557928c6
Kevin Le <kmle3@uci.edu>
2018-10-09 12:15:36 -0700

    Added seasonal WL info in !r6
    Added due to user feedback. Also added error message when user is not
    found.

--- game.py
+++ game.py
@@ -45,7 +45,11 @@ class GameCommands:
 	@commands.command()
 	async def r6(self, ctx, username):
  		r6auth = r6sapi.Auth(auth.r6username, auth.r6password)
- 		player = await r6auth.get_player(username, r6sapi.Platforms.UPLAY)
+ 		try:
+ 			player = await r6auth.get_player(username, r6sapi.Platforms.UPLAY)
+ 		except:
+ 			await ctx.send("Player was not found.")
+ 			return
  		rank = await player.load_rank("ncsa")
  		await player.load_level()
  		await player.load_general()
@@ -53,14 +57,16 @@ class GameCommands:
  		kdr = (player.kills) / (player.deaths)
  		kdar = (player.kills + player.kill_assists) / (player.deaths)
  		wlr = player.matches_won / player.matches_lost
+ 		swlr = rank.wins / rank.losses
  
  		embed = discord.Embed(title=username, description="Level {}".format(player.level), color=0xeee657)
  		embed.add_field(name="Player Profile", value=player.url, inline=False)
  		embed.add_field(name="Rank", value = "{}".format(rank.rank), inline=False)
  		embed.add_field(name="MMR", value = "Current: {} Max: {}".format(format(rank.mmr, '.2f'), format(rank.max_mmr, '.2f')), inline=False)
  		embed.add_field(name="Stats", value = "Kills: {} Assists: {} Deaths: {}".format(player.kills, player.kill_assists, player.deaths), inline=False)
- 		embed.add_field(name="Matches", value = "Won: {} Lost: {} Played: {}".format(player.matches_won, player.matches_lost, player.matches_played), inline=False)
- 		embed.add_field(name="KD Ratio", value = "KD: {} KDA: {} WL: {}".format(format(kdr, '.2f'), format(kdar, '.2f'), format(wlr, '.2f')), inline=False)
+ 		embed.add_field(name="Lifetime Matches", value = "Won: {} Lost: {} WL: {}".format(player.matches_won, player.matches_lost, format(wlr, '.2f')), inline=False)
+ 		embed.add_field(name="Seasonal Matches", value = "Won: {} Lost: {} WL: {}".format(rank.wins, rank.losses, format(swlr, '.2f')), inline=False)
+ 		embed.add_field(name="KD Ratio", value = "KD: {} KDA: {}".format(format(kdr, '.2f'), format(kdar, '.2f')), inline=False)
  		await ctx.send(embed=embed)
 
 def setup(bot):
--- misc.py
+++ misc.py
@@ -43,7 +43,7 @@ class MiscCommands:
 		embed.add_field(name="YouTube", value="Add options to play songs from list of search results", inline=False)
 		embed.add_field(name="MyAnimeList", value="Add more options when looking up anime/manga", inline=False)
 		embed.add_field(name="Game Stats", value="Add more options when looking up statistics", inline=False)
-		embed.add_field(name="Imageboards", value="Add image boards and options when requesting Dropbox images", inline=False)
+		embed.add_field(name="Imageboards", value="Add image boards", inline=False)
 		embed.add_field(name="Miscellaneous", value="Add strike system to keep track of how many infractions a user has", inline=False)
 		await ctx.send(embed=embed)
 
